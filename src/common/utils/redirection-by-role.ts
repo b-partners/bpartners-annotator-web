f5a688fe784bfced019ee10a5ec22a6bd00f10f6
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-12-01 09:16:43 +0300

    fix: redirection for admin loger

--- src/common/utils/redirection-by-role.ts
+++ src/common/utils/redirection-by-role.ts
@@ -1,13 +1,10 @@
 import { UserRole, Whoami } from 'bpartners-annotator-Ts-client';
 
 export const redirectionByRole = (whoami: Whoami | null) => {
-  if (whoami?.user?.role && whoami?.user?.team) {
-    switch (whoami.user?.role) {
-      case UserRole.ADMIN:
-        return `/teams/${whoami.user.team.id}/jobs`;
-      default:
-        return `/teams/${whoami.user.team.id}/jobs`;
-    }
+  switch (whoami?.user?.role) {
+    case UserRole.ADMIN:
+      return `/jobs`;
+    default:
+      return `/teams/${whoami?.user?.team?.id}/jobs`;
   }
-  return '/login';
 };
