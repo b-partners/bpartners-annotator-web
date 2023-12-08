5762696400e436e7b7ff9c843ca89e24d38f6167
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-12-08 13:16:22 +0300

    fix: scroll center

--- src/common/hooks/use-canvas-scale.ts
+++ src/common/hooks/use-canvas-scale.ts
@@ -13,8 +13,7 @@ export const useCanvasScale = (container: RefObject<HTMLDivElement>) => {
       const currentContainer = container.current;
       const middleY = (currentContainer.scrollHeight - currentContainer.clientHeight) / 2;
       const middleX = (currentContainer.scrollWidth - currentContainer.clientWidth) / 2;
-      currentContainer.scrollTop = middleY;
-      currentContainer.scrollLeft = middleX;
+      container.current.scrollTo({ left: middleX, top: middleY, behavior: 'instant' });
     }
   };
 
