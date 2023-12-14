0acf57cd56112132e0cde520f205c225e7c9b7b4
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-12-14 12:18:30 +0300

    fix: annotation colors

--- src/common/components/canvas/utilities.ts
+++ src/common/components/canvas/utilities.ts
@@ -4,3 +4,9 @@ export const getAnnotationsLastLabel = (annotations: IAnnotation[]) => {
   if (annotations.length === 0) return '';
   else return annotations[annotations.length - 1].label;
 };
+
+export const getAnnotationsLastColors = (annotations: IAnnotation[]) => {
+  if (annotations.length === 0) return null;
+  const { fillColor, strokeColor } = annotations[annotations.length - 1].polygon;
+  return { fillColor, strokeColor };
+};
