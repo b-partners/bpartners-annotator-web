83cb7c998cfd93c3cd6a4125274bafa956516e50
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-03 09:33:12 +0300

    chore: implement evaluation comment context

--- src/pages/admin/AdminTaskBoard.tsx
+++ src/pages/admin/AdminTaskBoard.tsx
@@ -6,6 +6,7 @@ import { CancelEvaluationButton, EvaluationRejectionButton, ValidateButton } fro
 import { Canvas } from '../../common/components/canvas';
 import { Sidebar } from '../../common/components/sidebar';
 import { CanvasAnnotationProvider } from '../../common/context';
+import { EvaluationCommentProvider } from '../../common/context/admin';
 import { dateFormater } from '../../common/utils';
 import { canvas_loading } from '../style';
 
@@ -14,47 +15,54 @@ export const AdminTaskBoard = () => {
     const [batch, setBatch] = useState(batchs[0] || {});
 
     return task !== null ? (
-        <CanvasAnnotationProvider batch={batch} img={task.imageUri || ''} labels={job?.labels || []}>
-            <Grid container height='94%' pl={1}>
-                <Grid
-                    container
-                    item
-                    xs={10}
-                    display='flex'
-                    direction='column'
-                    justifyContent='center'
-                    alignItems='center'
-                >
-                    <div>{job && <Canvas isLoading={false} job={job} />}</div>
-                    <Stack justifyContent='space-between' direction='row' width='70vh' mt={1}>
-                        <EvaluationRejectionButton />
-                        <ValidateButton />
-                    </Stack>
+        <EvaluationCommentProvider>
+            <CanvasAnnotationProvider batch={batch} img={task.imageUri || ''} labels={job?.labels || []}>
+                <Grid container height='94%' pl={1}>
+                    <Grid
+                        container
+                        item
+                        xs={10}
+                        display='flex'
+                        direction='column'
+                        justifyContent='center'
+                        alignItems='center'
+                    >
+                        <div>{job && <Canvas isLoading={false} job={job} />}</div>
+                        <Stack justifyContent='space-between' direction='row' width='70vh' mt={1}>
+                            <EvaluationRejectionButton />
+                            <ValidateButton />
+                        </Stack>
+                    </Grid>
+                    <Grid sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }} item xs={2}>
+                        <Stack flexGrow={2}>
+                            <List subheader={<ListSubheader>Versions de l'annotation</ListSubheader>}>
+                                <Stack pt={2} pb={3} px={2}>
+                                    <TextField
+                                        select
+                                        value={dateFormater(batch.creationDatetime)}
+                                        size='small'
+                                        fullWidth
+                                    >
+                                        {batchs.map(batch => {
+                                            const date = dateFormater(batch.creationDatetime);
+                                            return (
+                                                <MenuItem onClick={() => setBatch(batch)} key={date} value={date}>
+                                                    {date}
+                                                </MenuItem>
+                                            );
+                                        })}
+                                    </TextField>
+                                </Stack>
+                            </List>
+                            <Sidebar />
+                        </Stack>
+                        <Stack spacing={1} m={2} mb={1}>
+                            <CancelEvaluationButton />
+                        </Stack>
+                    </Grid>
                 </Grid>
-                <Grid sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }} item xs={2}>
-                    <Stack flexGrow={2}>
-                        <List subheader={<ListSubheader>Versions de l'annotation</ListSubheader>}>
-                            <Stack pt={2} pb={3} px={2}>
-                                <TextField select value={dateFormater(batch.creationDatetime)} size='small' fullWidth>
-                                    {batchs.map(batch => {
-                                        const date = dateFormater(batch.creationDatetime);
-                                        return (
-                                            <MenuItem onClick={() => setBatch(batch)} key={date} value={date}>
-                                                {date}
-                                            </MenuItem>
-                                        );
-                                    })}
-                                </TextField>
-                            </Stack>
-                        </List>
-                        <Sidebar />
-                    </Stack>
-                    <Stack spacing={1} m={2} mb={1}>
-                        <CancelEvaluationButton />
-                    </Stack>
-                </Grid>
-            </Grid>
-        </CanvasAnnotationProvider>
+            </CanvasAnnotationProvider>
+        </EvaluationCommentProvider>
     ) : (
         <Box sx={canvas_loading}>
             <CircularProgress />
