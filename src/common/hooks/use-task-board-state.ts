a131e1710634f9d26f046ff7da675310ba27e18e
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-05-15 17:05:44 +0300

    fix: annotation render on after validation

--- src/common/hooks/use-task-board-state.ts
+++ src/common/hooks/use-task-board-state.ts
@@ -1,20 +1,23 @@
-import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { IUseTaskBoardState } from '../../pages';
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
 import { teamJobsProvider, userTasksProvider } from '../../providers';
 import { userAnnotationsProvider } from '../../providers/annotator/user-annotations-provider';
-import { getLastCreatedAnnotationBatch } from '../../router/loaders';
+import { UserTaskLoader } from '../../router/loaders';
 import { cache, retryer } from '../utils';
 import { useFetch } from './use-fetch';
 
-export const useTaskBoardState = (props: IUseTaskBoardState) => {
+type UseTaskBoardFetchState = {
+    [K in keyof Pick<UserTaskLoader, 'annotationBatchs' | 'job' | 'task'>]: UserTaskLoader[K] | null;
+};
+
+export const useTaskBoardState = () => {
+    const { job, task, annotationBatchs } = useLoaderData() as UserTaskLoader;
+
     const params = useParams();
     const { user } = cache.getWhoami();
-    const [data, setState] = useState<IUseTaskBoardState>({ ...props });
 
     const navigate = useNavigate();
 
-    const fetcher = async () => {
+    const fetcher = async (): Promise<UseTaskBoardFetchState> => {
         const job = await retryer(teamJobsProvider.getOne(params?.teamId || '', params?.jobId || ''));
         if (!job || job.taskStatistics?.remainingTasksForUserId === 0) {
             navigate(`/teams/${params.teamId}/jobs`);
@@ -23,16 +26,23 @@ export const useTaskBoardState = (props: IUseTaskBoardState) => {
         if (!task) {
             navigate(`/teams/${params.teamId}/jobs`);
         }
-        const annotationBatchs = await retryer(userAnnotationsProvider.getBatchs(user?.id || '', task?.id || ''));
+        const annotationBatchs =
+            (await retryer(userAnnotationsProvider.getBatchs(user?.id || '', task?.id || ''))) || [];
 
-        if (!task) return setState({ task: null, job, annotationBatch: null });
+        if (!task) return { task: null, job, annotationBatchs };
 
-        const lastAnnotationBatch = getLastCreatedAnnotationBatch(annotationBatchs || []);
-
-        setState({ task, job, annotationBatch: lastAnnotationBatch });
+        return {
+            task,
+            job,
+            annotationBatchs,
+        };
     };
 
-    const { isLoading, fetcher: refetch } = useFetch({ fetcher });
+    const {
+        isLoading,
+        fetcher: refetch,
+        data: fetchResult = { annotationBatchs: [], job: null, task: null },
+    } = useFetch<UseTaskBoardFetchState>({ fetcher, defaultData: { job, task, annotationBatchs } });
 
-    return { ...data, changeState: refetch, isLoading };
+    return { changeState: refetch, isLoading, ...fetchResult };
 };
--- src/pages/TaskBoard.tsx
+++ src/pages/TaskBoard.tsx
@@ -2,7 +2,7 @@
 import { AnnotatorCanvas } from '@bpartners/annotator-component';
 import { Box, CircularProgress, Grid, MenuItem, Stack, TextField } from '@mui/material';
 import { useEffect } from 'react';
-import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { TaskReviewComment } from '../common/components/job&task';
 import { Sidebar } from '../common/components/sidebar';
 import {
@@ -20,7 +20,6 @@ import {
     useTaskBoardState,
 } from '../common/hooks';
 import { cache, dateFormater, isEmpty } from '../common/utils';
-import { UserTaskLoader } from '../router/loaders';
 import { canvas_loading } from './style';
 
 export const TaskBoard = () => {
@@ -29,22 +28,26 @@ export const TaskBoard = () => {
         setPolygons,
         annotations,
         setAnnotations,
-        annotationBatchs,
         annotationBatch,
         setBatchAnnotation,
+        annotationBatchs,
+        setBatchAnnotations,
     } = usePolygonAnnotationState();
 
-    const dataLoaded = useLoaderData() as UserTaskLoader;
-    const { changeState, isLoading, job, task } = useTaskBoardState(dataLoaded);
+    const { changeState, isLoading, job, task, annotationBatchs: fetchedAnnotationBatchs } = useTaskBoardState();
     const {
         annotationReviews: { annotationsReviews, globalReviews },
         fetchAnnotationReviews,
     } = useGetAnnotationReviews();
 
     useEffect(() => {
+        setBatchAnnotations(fetchedAnnotationBatchs || []);
+    }, [fetchedAnnotationBatchs]);
+
+    useEffect(() => {
         if (annotationBatch?.id && task?.id)
             fetchAnnotationReviews({ annotationBatchId: annotationBatch?.id, taskId: task.id });
-    }, [annotationBatch]);
+    }, [annotationBatch, annotationBatchs]);
 
     const params = useParams();
     const navigate = useNavigate();
@@ -70,7 +73,7 @@ export const TaskBoard = () => {
         <CanvasAnnotationProvider
             annotations={annotations}
             setAnnotations={setAnnotations}
-            img={task.imageUri || ''}
+            img={task?.imageUri || ''}
             batch={annotationBatch || undefined}
             annotationsReviews={annotationsReviews || undefined}
             globalReviews={globalReviews || undefined}
@@ -86,7 +89,7 @@ export const TaskBoard = () => {
                                 allowAnnotation={isUser()}
                                 height='70vh'
                                 width='70vw'
-                                image={task.imageUri || ''}
+                                image={task?.imageUri || ''}
                                 setPolygons={setPolygons}
                                 polygonList={polygons}
                             />
@@ -104,7 +107,7 @@ export const TaskBoard = () => {
                                 sx={{ mt: 2 }}
                                 fullWidth
                             >
-                                {annotationBatchs.map(batch => {
+                                {annotationBatchs?.map(batch => {
                                     const date = dateFormater(batch.creationDatetime);
                                     return (
                                         <MenuItem onClick={() => setBatchAnnotation(batch)} key={date} value={date}>
@@ -118,13 +121,17 @@ export const TaskBoard = () => {
                     </Stack>
                     <Stack spacing={1} m={2} mb={1}>
                         <CancelAnnotationButton />
-                        <NextAnnotationButton fetcher={changeState} task={task} />
-                        <ConfirmAnnotationButton
-                            isFetcherLoading={isLoading}
-                            task={task}
-                            onEnd={changeState}
-                            labels={job?.labels || []}
-                        />
+                        {task && (
+                            <>
+                                <NextAnnotationButton fetcher={changeState} task={task} />
+                                <ConfirmAnnotationButton
+                                    isFetcherLoading={isLoading}
+                                    task={task}
+                                    onEnd={changeState}
+                                    labels={job?.labels || []}
+                                />
+                            </>
+                        )}
                     </Stack>
                 </Grid>
             </Grid>
